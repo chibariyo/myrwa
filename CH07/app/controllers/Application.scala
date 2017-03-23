6799b6c3e524df30362e37d6587a48e5c280eee9
david <david.tateyama@become.co.jp>
2017-03-23 16:07:20 +0900

    Add login method to simply list all users.

--- CH07/app/controllers/Application.scala
+++ CH07/app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+import play.api.db._
+import play.api.i18n.{I18nSupport, MessagesApi}
+import org.jooq.{DSLContext, SQLDialect}
+import org.jooq.impl.DSL
+import generated.Tables._
+import generated.tables.records._
 import play.api.mvc._
 
-class Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+class Application @Inject() (val db: Database, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  def login = Action { implicit request =>
+    db.withConnection { connection =>
+      val sql: DSLContext =
+        DSL.using(connection, SQLDialect.POSTGRES_9_4)
+      val users = sql.selectFrom[UserRecord](USER).fetch()
+      Ok(users.toString)
+    }
   }
-
 }
